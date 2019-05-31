3ec613b02a2960679ef180e47acd8c7d2ea23a85
Vetal1977 <vitaly.bezgachev@gmail.com>
2019-05-31 07:44:50 +0200

    refactor: changed the at symbol

--- prometheus_metrics.go
+++ prometheus_metrics.go
@@ -1,4 +1,4 @@
-// Copyright 2019, Vitaly Bezgachev, vitaly.bezgachev@gmail.com, Kadir Tugan, kadir.tugan@gmail.com
+// Copyright 2019, Vitaly Bezgachev, vitaly.bezgachev [the_at_symbol] gmail.com, Kadir Tugan, kadir.tugan [the_at_symbol] gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
